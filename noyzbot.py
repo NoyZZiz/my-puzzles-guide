4b5ffdf3f6787a72f3acd1938a5047925ec1ee2d
NoyZZiz <155977430+NoyZZiz@users.noreply.github.com>
2025-01-23 09:16:22 +0000

    Update noyzbot.py

--- noyzbot.py
+++ noyzbot.py
@@ -38,4 +38,5 @@ def intro():
     return jsonify({"response": generate_intro()})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+
