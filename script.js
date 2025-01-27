e3badba5716c4fe0fa55f77f3a8c764bb45edb4d
NoyZZiz <155977430+NoyZZiz@users.noreply.github.com>
2025-01-27 15:02:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function fetchIntroMessage() {
-        fetch("http://127.0.0.1:5000/chat", { 
+        fetch("https://noyzbot-production.up.railway.app/chat", { 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ user_input: "intro" })
